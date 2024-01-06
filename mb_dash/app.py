cf81fe8ab4c3f43870f93507bebfb99a067b4f79
bigmb <malav.b93@gmail.com>
2024-01-06 07:49:01 +0000

    Bug fix commit

--- mb_dash/app.py
+++ mb_dash/app.py
@@ -9,40 +9,15 @@ external_css = ["https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap
 
 app = Dash(__name__, pages_folder='pages', use_pages=True, external_stylesheets=external_css)
 
-# app.layout = html.Div([
-# 	html.Br(),
-# 	html.P('Winnow Viz Web App', className="text-dark text-center fw-bold fs-1"),
-#     html.Div(children=[
-# 	    dcc.Link(page['name'], href=page["relative_path"], className="btn btn-dark m-2 fs-5")\
-# 			  for page in dash.page_registry.values()]
-# 	),
-# 	dash.page_container
-# ], className="col-8 mx-auto")
-
 app.layout = html.Div([
-    html.Br(),
-    html.P('Winnow Viz Web App', className="text-dark text-center fw-bold fs-1"),
+	html.Br(),
+	html.P('Winnow Viz Web App', className="text-dark text-center fw-bold fs-1"),
     html.Div(children=[
-        dcc.Link(page['name'], href=page["relative_path"], className="btn btn-dark m-2 fs-5") \
-        for page in dash.page_registry.values()
-    ]),
-    html.Div(id="page-content", className="col-8 mx-auto")
+	    dcc.Link(page['name'], href=page["relative_path"], className="btn btn-dark m-2 fs-5")\
+			  for page in dash.page_registry.values()]
+	),
+	dash.page_container
 ], className="col-8 mx-auto")
 
-# Define callback to update page content based on URL
-@app.callback(
-    Output("page-content", "children"),
-    [Input("url", "pathname")]
-)
-def display_page(pathname):
-    if pathname == "/data_load/":
-        return data_load.layout
-    elif pathname == "/profiler/":
-        return profiler.layout
-    else:
-        return "404 Page Not Found"
-
-
-
 if __name__ == '__main__':
-    app.run_server(mode='inline', port=8928,host='0.0.0.0')
+    app.run_server(port=8928,host='0.0.0.0')
