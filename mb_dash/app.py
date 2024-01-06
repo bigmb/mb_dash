1afd4c94bbdd615d1955d848dd4cf1eed32b7d16
bigmb <malav.b93@gmail.com>
2024-01-06 08:37:35 +0000

    Bug fix commit

--- mb_dash/app.py
+++ mb_dash/app.py
@@ -1,13 +1,12 @@
 from dash import Dash, html, dcc
 import dash
 import plotly.express as px
-from dash.dependencies import Input, Output
 
 px.defaults.template = "ggplot2"
 
 external_css = ["https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css", ]
 
-app = Dash(__name__, pages_folder='pages', use_pages=True, external_stylesheets=external_css)
+app = Dash(__name__, pages_folder='pages', use_pages=False, external_stylesheets=external_css)
 
 app.layout = html.Div([
 	html.Br(),
