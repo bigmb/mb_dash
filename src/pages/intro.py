ded9e699676eaae3668d94c5f0a3a37a68777843
bigmb <malav.b93@gmail.com>
2024-01-06 09:21:05 +0000

    Bug fix commit

--- src/pages/intro.py
+++ src/pages/intro.py
@@ -10,7 +10,7 @@ layout = html.Div(children=[
     ]),
     html.Div(children=[
         html.Br(),
-        html.H1("Step 1 :Loading Dataset"),
+        html.H3("Step 1 :Loading Dataset"),
         html.B("File path: "), "Enter the file path of the csv/parquet file",
         html.Br(),
         html.B("Dropdown : "), "Select the type of embedding to be used",
@@ -26,9 +26,13 @@ layout = html.Div(children=[
     ]),
     html.Div(children=[
         html.Br(),
-        html.H1("Step 2 : Check the Profiler"),
+        html.H3("Step 2 : Check the Profiler"),
         html.B("File Path: "), "Enter the file path of the csv/parquet file",
+        html.P("This step is used to check the data/distribution of the dataset. This step is optional."),
         html.Br(),
-    ])
+    ]),
+    html.Div(children=[
+        html.Br(),
+        html.H3("Step 3 : Visualize the Embeddings"),])
     
 ], className="bg-light p-4 m-2")
