d90867b3816874494571e09977a512ddb2eae8ab
bigmb <malav.b93@gmail.com>
2024-01-06 07:17:46 +0000

    Bug fix commit

--- mb_dash/pages/profiler.py
+++ mb_dash/pages/profiler.py
@@ -1,8 +1,27 @@
 ## dash file for profile of the loaded data
 
+import pandas as pd
 import dash
 from dash import dcc, html, callback
-from dash.dependencies import Input, Output, State
+import plotly.express as px
+from dash.dependencies import Input, Output
 
 
 dash.register_page(__name__, path='/', name="Dataset Profiler")
+
+#def create_distribution(col_name="Age"):
+#    return px.histogram(data_frame=None, x=col_name, height=600)
+
+columns = ["Age", "Fare", "SibSp", "Parch", "Survived", "Pclass", "Sex", "Embarked", "Cabin"]
+dd = dcc.Dropdown(id="dist_column", options=columns, value="Age", clearable=False)
+
+layout = html.Div(children=[
+    html.Br(),
+    html.P("Select Column:"),
+    dd,
+    dcc.Graph(id="histogram")
+])
+
+#@callback(Output("histogram", "figure"), [Input("dist_column", "value"), ])
+#def update_histogram(dist_column):
+#    return create_distribution(dist_column)
