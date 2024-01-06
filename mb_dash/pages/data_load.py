71c401b09414ae6296513f7bba3bba8b6f872099
bigmb <malav.b93@gmail.com>
2024-01-06 08:35:22 +0000

    Bug fix commit

--- mb_dash/pages/data_load.py
+++ mb_dash/pages/data_load.py
@@ -7,10 +7,11 @@ dash.register_page(__name__, path='/data_load', name="Loading Dataset")
 
 layout = html.Div([
     dcc.Input(id='file_path', type='text', placeholder='Enter the file path'),
+    html.Br(),
     dcc.Dropdown(
         id='dropdown1',
         options=[
-            {'label ': ' ', 'value': 'opt1'},
+            {'label ': '', 'value': 'opt1'},
             {'label ': 'PCA', 'value': 'opt2'},
             {'label': 'TSNE', 'value': 'opt3'},
             {'label': 'UMAP', 'value': 'opt4'},            
@@ -18,8 +19,13 @@ layout = html.Div([
         multi=False,
         value=[],
     ),
+    html.Br(),
     dcc.Input(id='taxcodes', type='text', placeholder='Enter the taxcodes separated by comma'),
+    html.Br(),
+    dcc.Input(id='taxcode_column_name', type='text', placeholder='Enter the taxcodes separated by comma'),
+    html.Br(),
     dcc.Input(id='file_save', type='text', placeholder='Enter the file save name'),
+    html.Br(),
     html.Button('Run Selection', id='run-button'),
     html.Div(id='output-div'),
 ])
@@ -31,24 +37,20 @@ layout = html.Div([
     [State('file_path', 'value'),
     State('dropdown1', 'value'),
     State('taxcodes', 'value'),
+    State('taxcode_column_name', 'value'),
     State('file_save', 'value')])
-def run_function(n_clicks, file_path, dropdown1, taxcodes,file_save):
+def run_function(n_clicks, file_path, dropdown1, taxcodes,taxcode_column_name,file_save):
     if n_clicks is None:
         return "Click the 'Run' button after making selections."
 
     if not any([file_path, dropdown1, taxcodes]):
         return "Please select at least one option or enter a string."
 
-    # Run your custom function using the selected values and string input
-    result = f"File Path: {file_path}"
-    result += f"Dropdown Selection: {', '.join(dropdown1)}<br>"
-    result += f"Taxcodes: {list(i.strip() for i in taxcodes.split(','))}"
-    result += f"File Save name: {file_save}"
+    taxcode_list = list(i.strip() for i in taxcodes.split(','))
 
     t1 = pd.read_csv(file_path)
     t1 = t1.dropna()
     t1 = t1.drop_duplicates()
     t1 = t1.reset_index(drop=True)
-    t1['taxcodes'] = t1['taxcodes'].isin(result['Taxcodes'])
+    t1[taxcode_column_name] = t1[taxcode_column_name].isin(taxcode_list)
     t1.to_csv(file_save,index=False)
-    return result
