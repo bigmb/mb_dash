7606d594379290a3649826700fb0581dfc9e0e7d
bigmb <malav.b93@gmail.com>
2024-01-06 07:38:58 +0000

    Bug fix commit

--- mb_dash/pages/data_load.py
+++ mb_dash/pages/data_load.py
@@ -6,37 +6,39 @@ from dash.dependencies import Input, Output, State
 dash.register_page(__name__, path='/', name="Loading Dataset")
 
 layout = html.Div([
+    dcc.Input(id='file_path', type='text', placeholder='Enter the file path'),
     dcc.Dropdown(
-        id='dropdown-1',
+        id='dropdown1',
         options=[
-            {'Type of ': 'Option 2', 'value': 'opt2'},
-            {'label': 'Option 3', 'value': 'opt3'},
+            {'label ': 'PCA', 'value': 'opt1'},
+            {'label': 'TSNE', 'value': 'opt2'},
+            {'label': 'UMAP', 'value': 'opt3'},            
         ],
         multi=True,
         value=[],
     ),
-    dcc.Input(id='file_path', type='text', placeholder='Enter the file path'),
+    dcc.Input(id='taxcodes', type='text', placeholder='Enter the taxcodes separated by comma'),
     html.Button('Run Selection', id='run-button'),
     html.Div(id='output-div'),
 ])
 
 
-
 @callback(
     Output('output-div', 'children'),
     [Input('run-button', 'n_clicks')],
-    [State('dropdown-1', 'value'),
-     State('file_path', 'value')]
-)
-def run_function(n_clicks, dropdown_1_values, dropdown_2_values, file_path):
+    [State('file_path', 'value'),
+    State('dropdown1', 'value'),
+    State('taxcodes', 'value')])
+def run_function(n_clicks, file_path, dropdown1, taxcodes):
     if n_clicks is None:
         return "Click the 'Run' button after making selections."
 
-    if not any([dropdown_1_values, dropdown_2_values, file_path]):
+    if not any([file_path, dropdown1, taxcodes]):
         return "Please select at least one option or enter a string."
 
     # Run your custom function using the selected values and string input
-    result = f"Dropdown 1 Selections: {', '.join(dropdown_1_values)}<br>"
     result += f"File Path: {file_path}"
+    result = f"Dropdown Selection: {', '.join(dropdown1)}<br>"
+    result += f"Tacodes: {list(i.strip() for i in taxcodes.split(','))}"
 
     return result
