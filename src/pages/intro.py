aeb10f579f5fb71bd723cdd215593cfb1de54a7e
bigmb <malav.b93@gmail.com>
2024-01-06 09:18:46 +0000

    Bug fix commit

--- src/pages/intro.py
+++ src/pages/intro.py
@@ -10,18 +10,25 @@ layout = html.Div(children=[
     ]),
     html.Div(children=[
         html.Br(),
-        html.H2("Loading Dataset"),
-        html.B("file path: "), "Enter the file path of the csv/parquet file",
+        html.H1("Step 1 :Loading Dataset"),
+        html.B("File path: "), "Enter the file path of the csv/parquet file",
         html.Br(),
-        html.B("dropdown: "), "Select the type of embedding to be used",
+        html.B("Dropdown : "), "Select the type of embedding to be used",
         html.Br(),
-        html.B("Taxcodes: "), "Taxcodes to be used for filtering the dataset",
+        html.B("Taxcodes : "), "Taxcodes to be used for filtering the dataset",
         html.Br(),
-        html.B("Embedding column name "), "Name of the column containing the embeddings",
+        html.B("Embedding column name :"), "Name of the column containing the embeddings",
         html.Br(),
-        html.B("Taxcode column name "), "Name of the column containing the taxcodes",
+        html.B("Taxcode column name : "), "Name of the column containing the taxcodes",
         html.Br(),
-        html.B("File save name "), "Name of the file to be saved with .csv extension",
+        html.B("File save name : "), "Name of the file to be saved with .csv extension",
+        html.Br(),
+    ]),
+    html.Div(children=[
+        html.Br(),
+        html.H1("Step 2 : Check the Profiler"),
+        html.B("File Path: "), "Enter the file path of the csv/parquet file",
         html.Br(),
     ])
+    
 ], className="bg-light p-4 m-2")
