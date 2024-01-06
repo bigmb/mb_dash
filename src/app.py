d333e914b2a105ca5b16a4a482a9d4d71637d066
bigmb <malav.b93@gmail.com>
2024-01-06 09:00:56 +0000

    Bug fix commit

--- src/app.py
+++ src/app.py
@@ -11,11 +11,10 @@ app = Dash(__name__, pages_folder='pages', use_pages=True, external_stylesheets=
 app.layout = html.Div([
 	html.Br(),
 	html.P('Winnow Viz Web App', className="text-dark text-center fw-bold fs-1"),
-    html.Div(children=[
-	    dcc.Link(page['name'], href=page["relative_path"], className="btn btn-dark m-2 fs-5")\
-			  for page in dash.page_registry.values()]
+    html.Div(children=[dcc.Link(page['name'], href=page["relative_path"], className="btn btn-dark m-2 fs-5") \
+                       for page in dash.page_registry.values()]
 	),
-	#dash.page_container
+	dash.page_container
 ], className="col-8 mx-auto")
 
 if __name__ == '__main__':
