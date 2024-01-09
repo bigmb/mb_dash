cbcdbd6d67846bc2a7e15c01863e33a49c9e6f75
Malav Bateriwala <malav.bateriwala@winnowsolutions.com>
2024-01-09 14:52:41 +0000

    Bug fix commit

--- src/pages/profile_create.py
+++ src/pages/profile_create.py
@@ -10,8 +10,8 @@ dash.register_page(__name__, path='/profiler', name="Dataset Profiler")
 
 layout = html.Div(children=[
     html.Br(),
-    dcc.Input(id='file_path_profiler', type='text', placeholder='Enter the file path'),
-    html.Button('Load File', id='execute_profie', n_clicks=0),
+    #dcc.Input(id='file_path_profiler', type='text', placeholder='Enter the file path'),
+    #html.Button('Load File', id='execute_profie', n_clicks=0),
     html.P("Select Column:"),
     #dcc.Dropdown(id="dist_column_profiler", value="Column", clearable=False),
     dcc.Dropdown(id="data_table_profile", value="Column", clearable=False),
@@ -19,34 +19,34 @@ layout = html.Div(children=[
 ])
 
 # Empty DataFrame for global variable
-load_db_profiler = pd.DataFrame(columns=['test_A', 'test_B', 'test_C'])
+# load_db_profiler = pd.DataFrame(columns=['test_A', 'test_B', 'test_C'])
 
-@callback(
-    #Output('dist_column_profiler', 'options'),
-    #Output('dist_column_profiler', 'value'),
-    Output("data_table_profile", "options"),
-    Output("data_table_profile", "value"),
-    [Input('execute_profie', 'n_clicks')],
-    [State('file_path_profiler', 'value')]
-)
-def update_dropdown_options(n_clicks, file_path_profiler):
-    global load_db_profiler
-    if n_clicks > 0 and file_path_profiler:
-        try:
-            # Load data from the specified file path
-            load_db_profiler = pd.read_csv(file_path_profiler)
-            columns = [{'label': col, 'value': col} for col in load_db_profiler.columns]
-            return columns, load_db_profiler.columns[0]  if columns else None
-        except Exception as e:
-            print(f"Error loading data: {e}")
+# @callback(
+#     #Output('dist_column_profiler', 'options'),
+#     #Output('dist_column_profiler', 'value'),
+#     [Output("data_table_profile", "options"),
+#     Output("data_table_profile", "value"),],
+#     [Input('execute_profie', 'n_clicks')],
+#     [State('file_path_profiler', 'value')]
+# )
+# def update_dropdown_options(n_clicks, file_path_profiler):
+#     global load_db_profiler
+#     if n_clicks > 0 and file_path_profiler:
+#         try:
+#             # Load data from the specified file path
+#             load_db_profiler = pd.read_csv(file_path_profiler)
+#             columns = [{'label': col, 'value': col} for col in load_db_profiler.columns]
+#             return columns, load_db_profiler.columns[0]  if columns else None
+#         except Exception as e:
+#             print(f"Error loading data: {e}")
     
-    # Return empty options and default value if no file path is provided or button not clicked
-    return [], None
+#     # Return empty options and default value if no file path is provided or button not clicked
+#     return [], None
 
 @callback(
     Output("histogram", "figure"),
     #[Input("dist_column_profiler", "value")
-    [Input("data_table_dataset", "value")]
+    [Input("store", "data")]
 )
-def update_histogram(dist_column):
-    return px.histogram(data_frame=load_db_profiler, x=dist_column, height=600)
+def update_histogram(data):
+    return px.histogram(data_frame=data['data1'],x=data['data1'].columns, height=600)
